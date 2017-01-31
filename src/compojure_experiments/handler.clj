261a3343b4f82b26e025a9ed1e8fa476229eae60
freefood89 <freefood89@gmail.com>
2017-01-31 13:41:01 +0000

    initial commit

---
+++ src/compojure_experiments/handler.clj
@@ -0,0 +1,54 @@
+(ns compojure-experiments.handler
+	(:require [compojure.core :refer :all]
+						[compojure.route :as route]
+						[ring.util.response :as response])
+	(:require [clojure.tools.logging :as log])
+	(:require [clojure.data.json :as json])
+	(:require [taoensso.carmine :as car :refer (wcar)]))
+
+(def server1-conn {:pool {} :spec {}}) ; See `wcar` docstring for opts
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+
+(defroutes app-routes
+	(GET "/" [] "Hello World")
+	(GET "/things/:id" [id] 
+		(do
+			(def ^:dynamic val (wcar* (car/get id)))
+			(if (nil? val)
+				(response/status
+					(response/response nil) 404)
+				(response/status
+					(response/content-type
+						(response/response
+							(json/write-str {:name val}))
+								"application/json") 200))))
+	(POST "/things" {body :body}
+		(do
+			(def ^:dynamic key (wcar* (car/incr "things")))
+			(if (nil? key)
+				(do
+					(log/error "Failed to Increment Key")
+					(response/status (response/response "") 500))
+				(do
+					(if
+						(= 
+							"OK" 
+							(wcar* (car/set key
+								(get (json/read-str (slurp body)) "name")))) ;need input validation
+						(response/status
+							(response/content-type
+								(response/response
+									(json/write-str {:key key}))
+										"application/json") 201)
+						(response/status
+							(response/response nil) 500))))))
+	(route/not-found "Not Found"))
+
+(defn wrap-logger [handler]
+	(fn [request]
+		(do
+			(log/info (get request :request-method) (get request :uri))
+			(handler request))))
+
+(def app
+	(wrap-logger app-routes))
